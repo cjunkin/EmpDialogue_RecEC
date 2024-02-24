6160e0398678de9ea760834a8b3dde36d56de6b3
Kumar Saunack <kr.saunack@gmail.com>
2024-02-23 18:16:57 -0800

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,20 +1,22 @@
-pip install -r EmpDialogue_RecEC/requirements.txt
+# pip install -r EmpDialogue_RecEC/requirements.txt
 
-wget https://nlp.stanford.edu/data/glove.6B.zip
-mv glove.6B.zip glove.6B.zip
-unzip -o glove.6B.zip
+# wget https://nlp.stanford.edu/data/glove.6B.zip
+# mv glove.6B.zip glove.6B.zip
+# unzip -o glove.6B.zip
 
-mkdir EmpDialogue_RecEC/outputs
-mkdir EmpDialogue_RecEC/outputs/emotion
+# mkdir EmpDialogue_RecEC/outputs
+# mkdir EmpDialogue_RecEC/outputs/emotion
 
-wget https://raw.githubusercontent.com/Tiiiger/bert_score/master/bert_score/rescale_baseline/en/roberta-large.tsv baseline.tsv
+# wget https://raw.githubusercontent.com/Tiiiger/bert_score/master/bert_score/rescale_baseline/en/roberta-large.tsv baseline.tsv
 
-if [ ! -d "/kaggle/working" ]; then
-  echo "On kaggle"
-  cp /kaggle/input/cs247-empathy-mental-health/best_emotion.pt EmpDialogue_RecEC/outputs/emotion/
-else
+current_dir=$(basename "$PWD")
+
+if [ "$current_dir" == "content" ]; then
   echo "On colab"
   cp /content/CS247-Empathy-Mental-Health/best_emotion.pt EmpDialogue_RecEC/outputs/emotion/
+else
+  echo "On kaggle"
+  cp /kaggle/input/cs247-empathy-mental-health/best_emotion.pt EmpDialogue_RecEC/outputs/emotion/
 fi
 
 echo "Setup done."
